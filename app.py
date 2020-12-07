ba9b4becf2eeab21d0fb2627a7f54fe29e0d065a
P H <pablohaedo@gmail.com>
2020-12-07 19:48:26 -0300

    change message

--- app.py
+++ app.py
@@ -39,10 +39,13 @@ def respond():
            # clear the message we got from any non alphabets
            text = re.sub(r"W", "_", text)
            # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+           url = "Lujaaan, no me escribas {}".format(text.strip())
            # reply with a photo to the name the user sent,
            # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+           bot.sendChatAction(chat_id=chat_id, action="typing")
+           sleep(1.5)
+           bot.send_message(chat_id=chat_id, reply_to_message_id=msg_id, text=text)
+           #bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
        except Exception:
            # if things went wrong
            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
@@ -63,7 +66,7 @@ def set_webhook():
 @app.route('/')
 def index():
     return '.'
-    
+
 if __name__ == '__main__':
     # note the threaded arg which allow
     # your app to have more than one thread
