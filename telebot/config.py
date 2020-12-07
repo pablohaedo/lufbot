4f5f33d20f8f3d555d130c1683b8818b6e011182
P H <pablohaedo@gmail.com>
2020-12-07 19:38:09 -0300

    add gunicorn

--- telebot/config.py
+++ telebot/config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MY_ENV_VAR = os.getenv('MY_ENV_VAR')
-
 BOT_TOKEN = os.getenv('BOT_TOKEN') 
 BOT_USERNAME = os.getenv('BOT_USERNAME')
 CALLBACK_URL = os.getenv('CALLBACK_URL')
