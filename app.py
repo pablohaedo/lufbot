3f59e3f1a5fe1636a957997df27bec2cc91d3399
Pablo Haedo <pablohaedo@gmail.com>
2020-12-21 17:17:41 -0300

    added message list

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import telegram
 import re
 from time import sleep
 from telebot.config import BOT_TOKEN, BOT_USERNAME, CALLBACK_URL
+from telebot.messages import messageList
 
 global bot
 global TOKEN
@@ -11,72 +12,87 @@ global TOKEN
 TOKEN = BOT_TOKEN
 bot = telegram.Bot(token=TOKEN)
 
+
 app = Flask(__name__)
+
 
 
 
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
-   # retrieve the message in JSON and then transform it to Telegram object
-   update = telegram.Update.de_json(request.get_json(force=True), bot)
+    # retrieve the message in JSON and then transform it to Telegram object
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-   print(update)
-   chat_id = update.message.chat.id
-   msg_id = update.message.message_id
+    print(update)
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id
 
-   #bot.sendMessage(chat_id=chat_id, text='bot_welcome', reply_to_message_id=msg_id)
-   
-   #return 'ok'
+    #bot.sendMessage(chat_id=chat_id, text='bot_welcome', reply_to_message_id=msg_id)
 
-   # Telegram understands UTF-8, so encode text for unicode compatibility
-   if update.message.text is None:
-       print('sale por falta de text???')
-       bot.sendMessage(chat_id=chat_id, text='que hago aca?', reply_to_message_id=msg_id)
-       return 'ok'
+    #return 'ok'
 
-   bot.sendMessage(chat_id=-492044533, text='MANDO SIEMPRE A ESTE CHAT')
+    # Telegram understands UTF-8, so encode text for unicode compatibility
+    if update.message.text is None:
+        print('sale por falta de text???')
+        bot.sendMessage(chat_id=chat_id, text='que hago aca?', reply_to_message_id=msg_id)
+        return 'ok'
 
-#    chatInfo = bot.getChat(chat_id)
-#    print(chatInfo)
-   text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
-   if text == "/start":
-       # print the welcoming message
-       bot_welcome = """
-       PH237, HOLA LUJAN
-       """
-       # send the welcoming message
-       
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-   else:
-       try:
-           # clear the message we got from any non alphabets
-           text = re.sub(r"W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "Lujaaan, no me escribas {}".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           if text == 'GOLPEAR':
-               url='Golpear no esta bien'
-           bot.sendChatAction(chat_id=chat_id, action="typing")
-           if text == 'ABRAZAR':
-               url='Love is love'
-           sleep(1.5)
-           keyboard=[['GOLPEAR','ABRAZAR']]
-           reply_markup = telegram.ReplyKeyboardMarkup(keyboard,
+    #    chatInfo = bot.getChat(chat_id)
+    #    print(chatInfo)
+    text = update.message.text.encode('utf-8').decode()
+    # for debugging purposes only
+    print("got text message :", text)
+    # the first time you chat with the bot AKA the welcoming message
+
+    if text in messageList:
+        bot.sendChatAction(chat_id=chat_id, action="typing")
+        nodo = messageList[text]
+        reply_markup = []
+        if 'keyboard' in nodo:
+            reply_markup = telegram.ReplyKeyboardMarkup([nodo['keyboard']],
                 one_time_keyboard=True,
                 resize_keyboard=True)
-           bot.send_message(chat_id=chat_id, reply_to_message_id=msg_id, text=url, reply_markup=reply_markup)
-           #bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception as error:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
-           print(error)
+        for message in nodo['messages']:
+            sleep(0.5)
+            bot.send_message(chat_id=chat_id, reply_to_message_id=msg_id, text=url, reply_markup=reply_markup)
+        return 'ok'
 
-   return 'ok'
+
+    if text == "/start":
+        # print the welcoming message
+        bot_welcome = """
+        PH237, HOLA LUJAN
+        """
+        # send the welcoming message
+        
+        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+
+    else:
+        try:
+            # clear the message we got from any non alphabets
+            text = re.sub(r"W", "_", text)
+
+            url = "Lujaaan, no me escribas {}".format(text.strip())
+            
+            if text == 'GOLPEAR':
+                url='Golpear no esta bien'
+            bot.sendChatAction(chat_id=chat_id, action="typing")
+            if text == 'ABRAZAR':
+                url='Love is love'
+            sleep(1.5)
+            keyboard=[['GOLPEAR','ABRAZAR']]
+            reply_markup = telegram.ReplyKeyboardMarkup(keyboard,
+                one_time_keyboard=True,
+                resize_keyboard=True)
+            bot.send_message(chat_id=chat_id, reply_to_message_id=msg_id, text=url, reply_markup=reply_markup)
+            #bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+        except Exception as error:
+            # if things went wrong
+            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+            print(error)
+
+    return 'ok'
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
