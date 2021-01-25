4bd1bfd6da365415a968f9ea24d6cdce4d47ef87
Pablo Haedo <pablohaedo@gmail.com>
2021-01-25 11:52:36 -0300

    add typing to log

--- telebot/services/logger.py
+++ telebot/services/logger.py
@@ -1,7 +1,8 @@
 from telebot.config import DEBUG
 from telebot.model.loglevel import LogLevel
+from typing import NoReturn
 
-def log(message, level = LogLevel.INFO):
+def log(message: str, level : LogLevel = LogLevel.INFO) -> NoReturn:
     if (not DEBUG) and (level < LogLevel.ERROR):
         return
     print(message)
